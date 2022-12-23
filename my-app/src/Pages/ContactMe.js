24f859751f2d9b00697c99e314f4d5a6c7bb635b
TMillsap04 <tmmillsap@student.fullsail.edu>
2022-12-22 21:48:19 -0600

    Restarting personal project

--- my-app/src/Pages/ContactMe.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function ContactMe() {
-  return (
-    <div>Ready to discuss some dollars?</div>
-  )
-}
-
-export default ContactMe
--- my-app/src/Pages/MyWork.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function MyWork() {
-  return (
-    <div>Let me show you some of what I can do</div>
-  )
-}
-
-export default MyWork
